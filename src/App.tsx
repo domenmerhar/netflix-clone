6e6c93e238accb2377038c041b941fb25694788a
unknown <merhardomen@gmail.com>
2024-02-06 19:50:57 +0100

    Added Row component with fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import requests from "./requests";
+
+import { Row } from "./Components/Row/Row";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Row/Row.tsx
@@ -0,0 +1,32 @@
+import { FC, useEffect, useState } from "react";
+import instance from "../../axios";
+
+type PropsType = { title: string; fetchUrl: string };
+
+export const Row: FC<PropsType> = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await instance.get(fetchUrl, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(response.data.results);
+
+      setMovies(response.data.results);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <section>
+      <h2>{title}</h2>
+    </section>
+  );
+};
