e5ecf6c15e6dfc4fa808b78e5a9257e3cc6e12e0
unknown <merhardomen@gmail.com>
2024-02-06 20:12:32 +0100

    Added width prop to MovieCard

--- src/Components/Movie Card/MovieCard.tsx
+++ src/Components/Movie Card/MovieCard.tsx
@@ -3,12 +3,13 @@ import { FC } from "react";
 type PropsType = {
   image: string;
   title: string;
+  width?: number;
 };
 
-export const MovieCard: FC<PropsType> = ({ image, title }) => {
+export const MovieCard: FC<PropsType> = ({ image, title, width = 200 }) => {
   return (
     <div>
-      <img src={image} alt={title} />
+      <img src={image} alt={title} style={{ width }} />
     </div>
   );
 };
