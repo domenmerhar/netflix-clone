2649ca8f9bbf87990217887199e05695b1cd4705
unknown <merhardomen@gmail.com>
2024-02-09 19:46:48 +0100

    Styled hero component

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import instance from "../../axios";
 import requests from "../../requests";
 import { MovieType } from "../../types";
-import { MovieCard } from "../Movie Card/MovieCard";
+
+import classes from "./Hero.module.css";
 
 export const Hero = () => {
   const [movie, setMovie] = useState<MovieType | null>(null);
@@ -25,17 +26,26 @@ export const Hero = () => {
   }, []);
 
   const backgroundImage =
-    movie && `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
+    movie && movie.backdrop_path
+      ? `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
+      : undefined;
+
+  console.log(backgroundImage);
 
   return (
     movie && (
-      <section style={{ backgroundImage }}>
-        <h1> {movie!.name}</h1>
+      <section
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+        className={classes.hero}
+      >
         <div>
-          <button>Play</button>
-          <button>My list</button>
+          <h1> {movie!.name}</h1>
+          <div>
+            <button>Play</button>
+            <button>My list</button>
+          </div>
+          <p>{movie!.overview}</p>
         </div>
-        <p>{movie!.overview}</p>
       </section>
     )
   );
