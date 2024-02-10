5ebbc178a4daafc532c4c8f77128bc2626c3d4c2
unknown <merhardomen@gmail.com>
2024-02-10 11:44:23 +0100

    Styled Navigation Bar

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { NetflixLogo } from "../NetflixLogo/NetflixLogo";
 
+import classes from "./NavigationBar.module.css";
+
 export const NavigationBar = () => {
   return (
-    <nav>
-      <NetflixLogo />
-      <img
-        src="https://wallpapers.com/images/hd/smiley-face-2560-x-1600-picture-qskp7rvw9msed949.jpg"
-        alt="Your Profile Picture"
-        width={64}
-        height={64}
-      />
+    <nav className={classes["navigation-bar"]}>
+      <div>
+        <NetflixLogo />
+        <img
+          src="https://wallpapers.com/images/hd/smiley-face-2560-x-1600-picture-qskp7rvw9msed949.jpg"
+          alt="Your Profile Picture"
+          width={64}
+          height={64}
+        />
+      </div>
     </nav>
   );
 };
